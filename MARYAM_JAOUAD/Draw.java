cc7d418512b982ad7f11b087d9da8390c4c75d32
maryam <jaouadmaryam19@gmail.com>
2024-03-01 13:52:46 +0000

    project submission

---
+++ MARYAM_JAOUAD/Draw.java
@@ -0,0 +1,20 @@
+public class Draw
+{
+    public static void main(String[] args)
+    {
+        GameArena arena = new GameArena(500, 300);
+        Ball b = new Ball(250, 150, 20, "GREEN");
+
+        arena.addBall(b);
+
+        while(true)
+        {
+            arena.pause();
+        }
+    }
+
+    /*
+     * description
+     * @param 
+     */
+}
